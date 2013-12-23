b1378c70b506553935b887d3f689cee24187adce
rory <rorymunro@hotmail.com>
2013-12-23 14:01:20 +0100

    add a staticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Musculation::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/rigime"
+  get "static_pages/forum"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
