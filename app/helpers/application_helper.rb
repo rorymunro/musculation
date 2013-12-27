5ea90d51a22adb57c8fdf29b05cda17d749d71de
rory <rorymunro@hotmail.com>
2013-12-27 16:25:15 +0100

    fucking up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 module ApplicationHelper
 
-	 # Returns the full title on a per-page basis.
-=======
-
-	module ApplicationHelper
-
   # Returns the full title on a per-page basis.
->>>>>>> filling-in-layout
   def full_title(page_title)
     base_title = "Musculation"
     if page_title.empty?
