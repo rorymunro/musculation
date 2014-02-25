d41518c9021d98d66e268ddb6fad6be7a4d6e1de
rory <rorymunro@hotmail.com>
2014-02-25 13:15:20 +0100

    jui

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,6 @@
 Musculation::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -84,8 +77,13 @@ config.force_ssl = true
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-  
 
-  
-end
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+  end
