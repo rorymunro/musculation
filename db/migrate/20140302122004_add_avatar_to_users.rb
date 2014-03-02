72f6cba44d8ea2eccd5381a58765da4397190e06
rory <rorymunro@hotmail.com>
2014-03-02 17:15:09 +0100

    deleted:    db/migrate/20140302122004_add_avatar_to_users.rb

--- db/migrate/20140302122004_add_avatar_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarToUsers < ActiveRecord::Migration
-  def self.up
-    add_attachment :users, :avatar, :force => true
-  end
-
-  def self.down
-    remove_attachment :users, :avatar
-  end
-end
